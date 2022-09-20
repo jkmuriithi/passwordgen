76a35612860a36e310d306a39fc28a91d072380c
Jude Muriithi <muriithi@princeton.edu>
2022-09-20 18:28:23 -0400

    Add comments

--- main.go
+++ main.go
@@ -11,8 +11,8 @@ import (
 
 const printable = "!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~"
 
-// Takes in a positive integer command line argument and returns a randomly
-// generated password
+// Takes in a positive integer length as a command line argument and returns a
+// randomly generated password of that length.
 func main() {
 	// Read command-line args
 	length, err := strconv.Atoi(os.Args[1])
@@ -23,6 +23,7 @@ func main() {
 		panic("Error: password_length must be a positive integer")
 	}
 
+	// Build password
 	pw := strings.Builder{}
 
 	chars := []rune(printable)
